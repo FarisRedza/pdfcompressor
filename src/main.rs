82fbcfff4af75dd6115a07013208e0834dec219f
FarisRedza <faris.redza1999@gmail.com>
2023-05-30 17:49:45 +0100

    changed multiple arg to args

--- src/main.rs
+++ src/main.rs
@@ -35,14 +35,16 @@ fn parse_input() -> (String, String, String) {
 
 fn compress(input_arg: String, output_arg: String, compress_arg: String) {
     Command::new("gs")
-        .arg("-sDEVICE=pdfwrite")
-        .arg("-dCompatibilityLevel=1.4")
-        .arg("-dNOPAUSE")
-        .arg("-dQUIET")
-        .arg("-dBATCH")
-        .arg(compress_arg)
-        .arg(output_arg)
-        .arg(input_arg)
+        .args([
+            "-sDEVICE=pdfwrite",
+            "-dCompatibilityLevel=1.4",
+            "-dNOPAUSE",
+            "-dQUIET",
+            "-dBATCH",
+            compress_arg.as_str(),
+            output_arg.as_str(),
+            input_arg.as_str(),
+        ])
         .spawn()
         .expect("ghostscript failed to execute");
 }
