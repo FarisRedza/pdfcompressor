798fff4918a66127f65fb6b562bcd61aa4a555b2
FarisRedza <faris.redza1999@gmail.com>
2023-05-31 21:01:29 +0100

    simplified args and flag options

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,41 @@ use std::io;
 use std::io::{Write, BufReader, BufRead, ErrorKind};
 use std::fs::File;
 use std::path::PathBuf;
+use std::process::Command;
 
-use clap::{Arg, Command};
+use clap::{Parser, Arg};
+
+/// Simple frontend for ghostscript to compress PDF files
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Compression level (low, medium, high)
+    #[arg(short, long, default_value_t = String::from("medium"))]
+    compression: String,
+
+    /// Input file name
+    #[arg(short, long)]
+    file: std::path::PathBuf,
+}
 
 fn main() {
-    let matches = Command::new("pdfcompressor")
-        .version("0.1.0")
-        .author("Faris Redza <faris.redza1999@gmail.com")
-        .about("A PDF file compressor frontend for ghostscript")
-        .arg(Arg::new("low quality")
-                .short('l')
-                .long("low")
-                .help("Output files will have up to 72 DPI"))
-        .arg(Arg::new("medium quality")
-                .short('m')
-                .long("medium")
-                .help("Output files will have up to 150 DPI"))
-        .arg(Arg::new("high quality")
-                .short('H')
-                .long("high")
-                .help("Output files will have up to 300 DPI"))
-        .get_matches();
+    let args = Args::parse();
+    let compress_arg = match args.compression.as_ref() {
+        "low" => String::from("-dPDFSETTINGS=/printer"),
+        "medium" => String::from("-dPDFSETTINGS=/ebook"),
+        "high" => String::from("-dPDFSETTINGS=/screen"),
+        _ => panic!("Error: Invalid compression level"),
+    };
+    
+    let file_check = args.file.clone().exists();
+    if file_check == true {
+        let output_file = args.file.clone().with_extension("compressed.pdf");
+    
+        let input_arg = String::from(args.file.into_os_string().into_string().unwrap());
+        let output_arg = format!("-sOutputFile={}", output_file.into_os_string().into_string().unwrap());
 
-    let (input_arg, output_arg, compress_arg) = pdfcompressor::parse_input();
-    pdfcompressor::compress(input_arg, output_arg, compress_arg);
+        pdfcompressor::compress(input_arg, compress_arg, output_arg);
+    } else {
+        panic!("Error: No file found");
+    }
 }
