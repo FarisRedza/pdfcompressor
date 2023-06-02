acde1991cf8fa3d736387cf3ba729eadb89abf61
FarisRedza <faris.redza1999@gmail.com>
2023-06-02 15:39:15 +0100

    WIP input parse rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use std::path::PathBuf;
+use std::{path::PathBuf};
 
 /// Simple frontend for ghostscript to compress PDF files
 #[derive(Parser, Debug)]
@@ -23,13 +23,24 @@ fn main() {
         _ => panic!("Error: Invalid output file quality, use low, medium, or high"),
     };
     
-    let file_check = args.file.clone().exists();
-    if file_check == true {
-        let input_arg = args.file.clone();
-        let output_arg = format!("-sOutputFile={}_compressed.pdf", args.file.with_extension("").into_os_string().into_string().unwrap());
+    let mut input_arg = match args.file.with_extension("").into_os_string().into_string() {
+        Err(why) => panic!("Failed to read file: {:?}", why),
+        Ok(input_arg) => input_arg,
+    };
 
-        pdfcompressor::compress(input_arg, compress_arg, output_arg);
-    } else {
-        panic!("Error: No file found");
-    }
+    let output_arg = input_arg + &String::from("_compressed.pdf");
+    println!("{}",input_arg);
+
+
+
+    // let input_arg = args.file.clone();
+
+    // let output_string = match args.file.with_extension("").into_os_string().into_string() {
+    //     Err(why) => panic!("Failed to convert output file path to String: {:?}", why),
+    //     Ok(output_string) => output_string,
+    // };
+
+    // let output_arg = format!("-sOutputFile={}_compressed.pdf", output_string);
+
+    // pdfcompressor::compress(input_arg, compress_arg, output_arg);
 }
